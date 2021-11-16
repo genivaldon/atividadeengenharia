121832a3b6e28a17567662c4369dc129c57fa0a1
ivanluciogba <ivanluciogba@gmail.com>
2021-11-16 20:54:58 -0300

    Nome

---
+++ AtividadeEng.java/Nome.java
@@ -0,0 +1,16 @@
+public class Nome {
+    private String nome;
+
+    public Nome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
